68e8bd75b80492408032de3930b05099d9a65951
ArhamDharewa <89764711+ArhamDharewa@users.noreply.github.com>
2022-01-04 10:24:27 +0530

    Go OFFICIAL

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -11,8 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common PixelExperience stuff
-TARGET_USES_AOSP_RECOVERY := true
+# PixelExtended Official Stuff
+PEX_BUILD_TYPE := OFFICIAL
+PEX_MAINTAINER := ArhamDharewa
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
